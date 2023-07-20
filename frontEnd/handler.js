1ba55c1d4b100b2c77be9f443bb8241116dad856
amine <minoplay07@gmail.com>
2023-07-20 22:56:21 +0100

    starting making user interface - signup page

---
+++ frontEnd/handler.js
@@ -0,0 +1,43 @@
+const form = document.forms[0];
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = DOMPurify.sanitize(form.querySelector("#username").value);
+    const email = DOMPurify.sanitize(form.querySelector("#email").value);
+    const password = DOMPurify.sanitize(form.querySelector("#password").value);
+    // validate input
+    if (
+        validator.isEmpty(username) ||
+        validator.isEmpty(password) ||
+        validator.isEmpty(email)
+    ) {
+        throw new Error("all fields are required");
+    }
+    if (!validator.isEmail(email)) {
+        throw new Error("Email format is incorrect");
+    }
+    const jsonObj = {
+        username,
+        email,
+        password,
+    };
+
+    try {
+        const response = await fetch("http://localhost:5001/api/users/register", {
+            method: "POST",
+            body: JSON.stringify(jsonObj),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+
+        const data = await response.json();
+        console.log(JSON.stringify(data));
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+});
--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
